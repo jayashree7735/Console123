2d223dcbc43ad2ac419162ea2a7cecadea1f9094
Jayashree Rout <jrout911@gmail.com>
2024-10-30 10:08:49 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using System.ComponentModel.Design;
+
+namespace dotnet_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* Console.WriteLine("Hello, World!");*/
+
+            //int cube;
+            //Console.WriteLine("Enter the value=");
+            //cube = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("The cube of the Given no ="+(cube* cube * cube));
+
+
+            //int Marks;
+            //Console.WriteLine("Enter the mark=");
+            //Marks = Convert.ToInt32(Console.ReadLine());
+            //if (Marks > 50)
+            //{
+            //    Console.WriteLine("pass");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("fail");
+            //}
+
+            //            if (Marks > 450)
+            //            {
+            //                Console.WriteLine("First class");
+            //            }
+            //            else if (Marks > 250)
+            //            {
+            //                Console.WriteLine("Second class");
+            //            }
+            //            else if (Marks > 150)
+            //            {
+            //                Console.WriteLine("Second class");
+            //            }
+            //            else
+            //            {
+            //                Console.WriteLine("fail");
+            //            }
+            //        }
+            //    }
+            //}
+
+            //int Num;
+            //Console.WriteLine("Enter the Number=");
+            //Num = Convert.ToInt32(Console.ReadLine());
+
+            //if (Num < 0)
+            //{
+            //    Console.WriteLine("negetive");
+            //}
+            //else if (Num == 0)
+            //{
+            //    Console.WriteLine("zero");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("positive");
+            ////}
+
+            //int a, b, c;
+            //Console.WriteLine("Enter the 1st no=");
+            //a = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter the 2ndt no=");
+            //b = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter the 3rdt no=");
+            //c = Convert.ToInt32(Console.ReadLine());
+
+            //if (a > b)
+            //{
+            //    if (a > c)
+            //    {
+            //        Console.WriteLine("a is big");
+            //    }
+
+            //    else (b > c)
+            //        {
+            //        Console.WriteLine("b is big");
+            //    }
+//            //}
+                   
+//        }
+
+//    }
+//}
---
+++ Usbm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dotnet_project;
+using class_2;
+
+
+namespace Class_1
+{
+    internal class Usbm : class_2.Program
+    {
+        static void Main(string[] args)
+        {
+            Usbm obj = new Usbm();
+            obj.show2();
+            obj.show5();
+           
+        }
+    }
+}
